7452985dbd74d363e60afb3caf341516465fd994
Krāv <chris@kravtofly.com>
2025-08-19 16:25:26 -0500

    Update check-account-status.js

--- api/check-account-status.js
+++ api/check-account-status.js
@@ -2,7 +2,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 module.exports = async function handler(req, res) {
   // Enable CORS
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.kravtofly.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
