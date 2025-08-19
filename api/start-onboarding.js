e324dec54b8658d15464c5032a1709610b472f60
Krāv <chris@kravtofly.com>
2025-08-19 16:26:06 -0500

    Update start-onboarding.js

--- api/start-onboarding.js
+++ api/start-onboarding.js
@@ -2,7 +2,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 module.exports = async function handler(req, res) {
   // Enable CORS for your Webflow domain
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.kravtofly.com');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
